b149ebca5cabb6c518ca8df24bdf1a78233b30c1
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-07-04 20:03:16 +0300

    v6.0.4

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -3176,7 +3176,7 @@ function createExamInfoSubmenu(basicRows, onShowFull, closeDropdown, reposition)
   const trigger = document.createElement("button");
   trigger.type = "button";
   trigger.className = "exam-action-btn submenu-trigger";
-  trigger.innerHTML = `<span class="submenu-trigger-label">${INFO_ICON_SVG}<span>معلومات الإمتحان</span></span><svg class="submenu-caret" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="m9 18 6-6-6-6"/></svg>`;
+  trigger.innerHTML = `<span class="submenu-trigger-label">${INFO_ICON_SVG}<span>معلومات الإمتحان</span></span><svg class="submenu-caret" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="m15 18-6-6 6-6"/></svg>`;
 
   const content = document.createElement("div");
   content.className = "submenu-content";
