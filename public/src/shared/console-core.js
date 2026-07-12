8d13e767b1e6a13e390c2036aadddaca54c8209e
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-07-12 16:26:27 +0300

    Updated version of service worker

--- public/src/shared/console-core.js
+++ public/src/shared/console-core.js
@@ -164,7 +164,7 @@ const LOGO = "منصة إمتحانات بصمجي";
   }
 
   // This should dynamically pull the version from the service-worker, or from the package.json if possible.
-  const BUILD_TAG = 'core@6.0.10';
+  const BUILD_TAG = 'core@6.0.11';
 
   function printHelp() {
     log.rule('═', 64);
