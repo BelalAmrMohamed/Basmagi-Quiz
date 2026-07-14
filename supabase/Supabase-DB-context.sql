a354a840365d71272793217ae1540032126ead97
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-07-15 01:41:04 +0300

    update 6.0.23

--- supabase/Supabase-DB-context.sql
+++ supabase/Supabase-DB-context.sql
@@ -1,4 +1,4 @@
--- Supabase-DB-context.sql | Last updated on version `v6.0.22`
+-- Supabase-DB-context.sql | Last updated on version `v6.0.23`
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
