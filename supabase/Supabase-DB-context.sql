fabe3928ed192ddce9a81d2de46f6e6cc504f365
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-07-20 09:32:59 +0300

    v6.1.24 | Profile Page | Fixed Issues #4

--- supabase/Supabase-DB-context.sql
+++ supabase/Supabase-DB-context.sql
@@ -1,4 +1,4 @@
--- Supabase-DB-context.sql | Last updated on version `v6.1.23`
+-- Supabase-DB-context.sql | Last updated on version `v6.1.24`
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
