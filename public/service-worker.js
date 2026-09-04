e75bf0066244aa2f8a00d25ae3ec42ee020697dc
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-09-04 07:39:36 +0300

    incremented version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker for Basmagi Quiz Platform
 
-const CACHE_VERSION = "basmagi-v8.0.1";
+const CACHE_VERSION = "basmagi-v8.1.0";
 const OFFLINE_CACHE = `${CACHE_VERSION}-offline`;
 const OFFLINE_URL = "/offline.html";
 const OFFLINE_ASSETS = [OFFLINE_URL, "/favicon.png"];
