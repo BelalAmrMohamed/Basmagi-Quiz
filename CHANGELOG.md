faf0a2011e394cc7f68b12e9145511fe0f0b6ea8
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-09-12 15:28:19 +0300

    Increamented version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ---
 
-## [8.2.9] - 2026-9-9
+## [8.3.0] - 2026-9-9
 
 ### Updated og.js
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker for Basmagi Quiz Platform
 
-const CACHE_VERSION = "basmagi-v8.2.9";
+const CACHE_VERSION = "basmagi-v8.3.0";
 const OFFLINE_CACHE = `${CACHE_VERSION}-offline`;
 const OFFLINE_URL = "/offline.html";
 const OFFLINE_ASSETS = [OFFLINE_URL, "/favicon.png"];
